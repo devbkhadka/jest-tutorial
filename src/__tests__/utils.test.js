6836d7ac96ec07388a784605d4cc7519d6d5cd90
Dev Khadka <devbahadurkhadka@SMS-EXE-Dev-2.local>
2020-04-12 17:27:34 +0545

    implemented fetch restaurants feature

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -26,26 +26,5 @@ describe('fetchRestaurants', ()=>{
 })
 
 describe('calculateDistance', ()=>{
-    it('should return distance in meters from center to a location given in degree', ()=>{
-        const testLocationPairs = [
-            [ 40.76404704,-73.98364954],
-            [ 26.212754, 84.961525],
-            [27.699363, 85.325500],
-            [ -11.166805, 38.408597],
-        ]
-        const expectedDistances = [
-            12109725,
-            168479,
-            1181,
-            6647488,
-        ]
-
-        const calculatedDistances = testLocationPairs.map((location)=>{
-            return calculateDistance(location)
-        })
-
-        // Test calculated values with in 1km range of expected value
-        expect(calculatedDistances.map(d=>Math.floor(d/100)))
-            .toEqual(expectedDistances.map(d=>Math.floor(d/100)))
-    })
+    
 })
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,4 @@
 export const RESTAURANTS_URL = "https://gist.githubusercontent.com/devbkhadka/39301d886bb01bca84832bac48f52cd3/raw/f7372da48797cf839a7b13e4a7697b3a64e50e34/restaurants.json"
-export const CENTER_LOCATION = [27.690870, 85.332701]
-const EARTH_RADIUS_KM = 63710
-const PI_RADIAN_IN_DEGREE = 180
 
 export async function fetchRestaurants() {
     try{
@@ -16,17 +13,5 @@ export async function fetchRestaurants() {
 }
 
 export function calculateDistance(location){
-    const [x1, y1] = convertCoordinateFromDegreeToRadian(location)
-    const [x2, y2] = convertCoordinateFromDegreeToRadian(CENTER_LOCATION)
-
-    const term1 = Math.sin((x2-x1)/2)**2
-    const term2 = Math.sin((y2-y1)/2)**2 * Math.cos(x1) * Math.cos(x2)
-
-    const distance = 2*EARTH_RADIUS_KM*Math.asin(Math.sqrt(term1+term2))
-    return distance * 100
-}
-
-function convertCoordinateFromDegreeToRadian(point) {
-    const [x, y] = point
-    return [x*Math.PI/PI_RADIAN_IN_DEGREE, y*Math.PI/PI_RADIAN_IN_DEGREE]
+    
 }
